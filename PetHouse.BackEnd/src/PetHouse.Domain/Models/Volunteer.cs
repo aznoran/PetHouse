c2d626fcb3f976b1a152e4f8067feb5b170e693e
aznoranton <savchenko.a.r@edu.mirea.ru>
2024-08-10 08:02:32 +0300

    Added Volunteer.cs and SocialNetwork.cs

---
+++ PetHouse.BackEnd/src/PetHouse.Domain/Models/Volunteer.cs
@@ -0,0 +1,27 @@
+﻿namespace PetHouse.Domain;
+
+public class Volunteer
+{
+    public Guid Id { get; private set; }
+    
+    public string FullName { get; private set; }
+    
+    public string Description { get; private set; }
+    
+    public short YearsOfExperience { get; private set; }
+    
+    public int CountOfPetsFoundHome { get; private set; }
+    
+    public int CountOfPetsLookingForHome { get; private set; }
+    
+    public int CountOfPetsOnTreatment { get; private set; }
+    
+    public string PhoneNumber { get; private set; }
+    
+    public ICollection<SocialNetwork> SocialNetworks { get; private set; }
+    
+    public ICollection<Requisite> Requisites { get; private set; }
+    
+    public ICollection<Pet> Pets { get; private set; }
+    
+}
