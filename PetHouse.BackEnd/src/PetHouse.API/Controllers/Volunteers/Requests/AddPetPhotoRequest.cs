d809416f6c42e0fa622cec3e48889daea82304fe
aznoranton <savchenko.a.r@edu.mirea.ru>
2024-10-01 09:20:25 +0300

    .

--- PetHouse.BackEnd/src/PetHouse.API/Controllers/Volunteers/Requests/AddPetPhotoRequest.cs
+++ PetHouse.BackEnd/src/PetHouse.API/Controllers/Volunteers/Requests/AddPetPhotoRequest.cs
@@ -1,6 +1,5 @@
 using PetHouse.Application.Dtos.Shared;
 using PetHouse.Application.PetManagement.Commands.AddPetPhoto;
-using PetHouse.Application.PetManagement.Commands.AddPetPhotos;
 
 namespace PetHouse.API.Controllers.Volunteers.Requests;
 
